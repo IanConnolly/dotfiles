177e78c793a9f1262bccf8769ff64f130bd34b81
Ian Connolly <ian@scribd.com>
2016-09-11 14:39:29 -0700

    Save keymaps

--- keymaps/keymap_ianconnolly_osx.c
+++ keymaps/keymap_ianconnolly_osx.c
@@ -7,9 +7,7 @@
 
 // Differences from OSX Defaults:
 // Esc-on-press,Ctrl-on-hold now on to the left of the left-home row for Vim
-// My tmux-prefix (Ctrl-a) now on left hand
 // Right-hand index finger symbol toggle now a modifier for chording with the left hand
-// Arrow keys removed and extra programming symbols put onto the base layer
 
 #include "ergodox_ez.h"
 #include "debug.h"
@@ -23,45 +21,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |   '  |           |   )  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   =  |           |  L1  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |   (    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  ~L1 |   Y  |   U  |   I  |   O  |   P  |  LCmd  |
+ * |   %    |   Q  |   W  |   E  |   R  |   T  | [ / {|           | ] / }|   Y  |   U  |   I  |   O  |   P  |  | / \ |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CtlEsc |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   :    |
- * |--------+------+------+------+------+------| TmuxP|           | Hyp  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | CtlEsc |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '"   |
+ * |--------+------+------+------+------+------| Cmd  |           | Cmd  |------+------+------+------+------+--------|
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf|  {   |   }  |                                       |   \  |   @  |   [  |   ]  | ~L1  |
+ *   | Alt  | Mute | VolDn| VolUp|  F1  |                                       |  F2  | Left | Down |  Up  | Right  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LCmd |       | Alt  |Ctrl/Esc|
+ *                                        | Alt |CtrlEsc|       |CtrlEsc|  Alt  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       |CmdTab|        |      |
+ *                                 |      |      | Home |       | PgUp |        |      |
  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       |CmdSpc|        |      |
+ *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_QUOT,
-        KC_LPRN,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_EQL,
+        KC_PERC,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LBRC,
         CTL_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   LCTL(KC_A),
-        LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LCBR,KC_RCBR,
-                                              ALT_T(KC_APP),  KC_LGUI,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LGUI,
+        KC_LALT,        KC_MUTE,      KC_VOLD,KC_VOLU,KC_F1,
+                                               KC_LGUI,       KC_LALT,
                                                               KC_HOME,
                                                KC_SPC,KC_BSPC,KC_END,
         // right hand
-             KC_RPRN,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             KC_FN1,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LGUI,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_COLON,
-             ALL_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_BSLS,KC_AT,  KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             LGUI(KC_TAB),
-             LGUI(KC_SPC),KC_TAB, KC_ENT
+             TG(1),       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
+             KC_RBRC,KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
+                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_QUOT,
+             KC_LGUI,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+                                  KC_F2,  KC_LEFT,KC_DOWN,KC_UP,            KC_RIGHT,
+             CTL_T(KC_ESC), KC_LALT,
+             KC_PGUP,
+             KC_PGDN,KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
